d7989847baef5da5586155114a683629d91c1659
Pavel Yuzov <p.yuzov@gmail.com>
2020-06-15 20:55:25 +0700

    add load moves from filefrom file, cut a chess piece

--- src/main.c
+++ src/main.c
@@ -4,19 +4,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+
 char b[8][8];
 char abcIdx[8] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
-int main()
+int main(int argc, char* argv[])
 {
     initBoard(b);
     char* input = NULL;
-    do {
-        system("clear");
-        printBoard(b, abcIdx);
-        printf("\nВведите ход или 'n' для выхода'\n");
-        input = boardRead();
-        if (strcmp(input, "n") != 0) {
-            move(input, b);
+    if (argc==1){    
+        do {
+            system("clear");
+            printBoard(b, abcIdx);
+            printf("\nВведите ход или 'n' для выхода'\n");
+            input = boardRead();
+            if (strcmp(input, "n") != 0) {
+                move(input, b);
+            }
+        } while (strcmp(input, "n") != 0);
+    }
+    else{
+        FILE* file;
+        file=fopen(argv[1],"r");
+        if (file==NULL)
+        {
+            printf("\nfile is null");
+
         }
-    } while (strcmp(input, "n") != 0);
+        else
+        {
+            input = malloc(12);
+            while (fgets(input,12,file)!=NULL){
+                move(input, b);
+            }
+            printBoard(b,abcIdx);
+            fclose(file);
+        }
+        
+    }
 }
