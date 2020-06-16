a572a4101f2dac8d44d9faabdd9b07db91bc60b4
Pavel Yuzov <p.yuzov@gmail.com>
2020-06-16 15:55:10 +0700

    edit travis.yml

--- src/board_print_plain.c
+++ src/board_print_plain.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void printBoard(char b[8][8], char abcIdx[8]) {
-  for (int i = 7; i >= 0; i--) {
-    printf("\n%i", i + 1);
-    for (int j = 0; j < 8; j++) {
-      printf("%c", b[i][j]);
+void printBoard(char b[8][8], char abcIdx[8])
+{
+    for (int i = 7; i >= 0; i--) {
+        printf("\n%i", i + 1);
+        for (int j = 0; j < 8; j++) {
+            printf("%c", b[i][j]);
+        }
     }
-  }
-  printf("\n ");
-  for (int i = 0; i < 8; i++) {
-    printf("%c", abcIdx[i]);
-  }
+    printf("\n ");
+    for (int i = 0; i < 8; i++) {
+        printf("%c", abcIdx[i]);
+    }
 }
