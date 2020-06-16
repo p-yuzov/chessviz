93aa63914d53de19dcc6dff962854ba6de8e5d8a
Pavel Yuzov <p.yuzov@gmail.com>
2020-06-16 21:26:19 +0700

    add tests checkInput_len11_return0 checkInput_NULL_return_minus1

--- src/board.h
+++ src/board.h
@@ -2,4 +2,5 @@
 #define H_BOARD
 void initBoard(char b[8][8]);
 void move(char *input, char b[8][8]);
+int checkInput(char* input);
 #endif
--- test/main.c
+++ test/main.c
@@ -1,7 +1,17 @@
 #define CTEST_MAIN
 #include <../thirdparty/ctest.h>
-
+#include <../src/board.h>
 int main(int argc, const char** argv)
 {
     return ctest_main(argc, argv);
+}
+CTEST(board_suit, checkInput_len11_return0){
+    char *input = {'e','2','-','e','4',' ','e','7','-','e','6'};
+    int result = checkInput(input);
+    ASSERT_EQUAL(0,result);
+}
+CTEST(board_suit, checkInput_NULL_return_minus1){
+    char *input = NULL;
+    int result = checkInput(input);
+    ASSERT_EQUAL(-1,result);
 }
